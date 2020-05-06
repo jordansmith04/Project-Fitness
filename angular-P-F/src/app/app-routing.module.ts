500b706ac30e6e8463575a1c05bda864ab02a79a
cfor0 <cfor0@outlook.com>
2020-05-06 09:44:32 -0400

    changed a few pics and linked tables to home page buttons

--- angular-P-F/src/app/app-routing.module.ts
+++ angular-P-F/src/app/app-routing.module.ts
@@ -5,13 +5,23 @@ import { RegisterComponent } from './components/register/register.component';
 import { WorkoutsComponent } from './components/workouts/workouts.component';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ChestArmsComponent } from './components/chest-arms/chest-arms.component';
+import { LegsComponent } from './components/legs/legs.component';
+import { BackShoulderComponent } from './components/back-shoulder/back-shoulder.component';
+import { AbsComponent } from './components/abs/abs.component';
+import { StretchComponent } from './components/stretch/stretch.component';
 
 
 const routes: Routes = [
   {path:'login', component: LoginComponent},
   {path:'register', component: RegisterComponent},
   {path: '', component: WorkoutsComponent},
-  {path: '', component: NavbarComponent, outlet:"navbar"}
+  {path: '', component: NavbarComponent, outlet:"navbar"},
+  {path:'chestArms', component: ChestArmsComponent},
+  {path:'legs', component: LegsComponent},
+  {path:'backShoulder', component: BackShoulderComponent},
+  {path:'Abs', component: AbsComponent},
+  {path:'stretches', component: StretchComponent}
 ];
 
 @NgModule({
@@ -20,4 +30,5 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-export const routingComponents = [LoginComponent, RegisterComponent, WorkoutsComponent, NavbarComponent]
+export const routingComponents = [LoginComponent, RegisterComponent, WorkoutsComponent, NavbarComponent,
+   ChestArmsComponent, LegsComponent, BackShoulderComponent, AbsComponent, StretchComponent]
