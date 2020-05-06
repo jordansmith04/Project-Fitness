143014aa0e2511f35c5f88ff3bfa2530b4216a74
cfor0 <cfor0@outlook.com>
2020-05-06 15:44:35 -0400

    s

--- angular-P-F/src/app/services/register.service.ts
+++ angular-P-F/src/app/services/register.service.ts
@@ -8,7 +8,7 @@ import {User} from '../model/User'
 })
 export class RegisterService {
 
-  url = 'http://localhost:8080/user/userinfo';
+  url = 'http://localhost:8080/user/NewUser';
 
   constructor(private http: HttpClient) { 
 
